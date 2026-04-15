ba7e91f12cf023d771fb0f31eb5c02165424d090
smeron659-art <smeron659@gmail.com>
2026-04-15 15:21:56 +0300

    the use of the  taskcontex and addd the setting folder

--- recored.js
+++ recored.js
@@ -1,97 +1,106 @@
-
-import { View,Text, StyleSheet,TouchableOpacity,ImageBackground} from 'react-native';
-import {TextInput} from 'react-native-paper';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ImageBackground,
+} from "react-native";
+import { TextInput } from "react-native-paper";
 import { SystemBars } from "react-native-edge-to-edge";
-import React, { useState,useEffect } from "react";
-export default  function Recored({ focuse,back,RecorederTime}){
-    const [teker,setTeker]=useState([]);
-    const[timestore,setTimestore]=useState([])
-    useEffect(()=>{
-        if (!focuse) return;
-setTeker(prev=>[...prev,focuse])
-    },[focuse]) 
- useEffect(()=>{
+import React, { useState, useEffect } from "react";
+export default function Recored({ focuse, back, RecorederTime }) {
+  const [teker, setTeker] = useState([]);
+  const [timestore, setTimestore] = useState([]);
+  useEffect(() => {
+    if (!focuse) return;
+    setTeker((prev) => [...prev, focuse]);
+  }, [focuse]);
+  useEffect(() => {
     if (!RecorederTime) return;
-setTimestore(prev=>[...prev,RecorederTime])
-    },[RecorederTime])
-    return(
-    <View style={styles.continer}> 
-<Text style={styles.title}> Your Recoreds page </Text>
-<View style={styles.text }>
-<Text  style={styles.title}> ExerciseType</Text> <Text style={{ marginHorizontala:50}}>  </Text> <Text style={styles.titlel}> Time Takens</Text>
-</View>
- <SystemBars style="light" />
-<ImageBackground  source={require('./assets/light.jpg')} style={styles.image} >
- {teker.map((item,index)=> (
-    <View key={index} style={{flexDirection:'row'}}>
-      <Text style={{marginHorizontal:50,color:'#fff',fontSize:20}}>{item}</Text>
-      <Text style={{color:'#fff',fontSize:20}}>{timestore[index]}</Text>
-    </View>
-  ))
-  }
-  </ImageBackground>  
+    setTimestore((prev) => [...prev, RecorederTime]);
+  }, [RecorederTime]);
+  return (
+    <View style={styles.continer}>
+      <Text style={styles.title}> Your Recoreds page </Text>
+      <View style={styles.text}>
+        <Text style={styles.title}> ExerciseType</Text>{" "}
+        <Text style={{ marginHorizontala: 50 }}> </Text>{" "}
+        <Text style={styles.titlel}> Time Takens</Text>
+      </View>
+      <SystemBars style="light" />
+      <ImageBackground
+        source={require("./assets/light.jpg")}
+        style={styles.image}
+      >
+        {teker.map((item, index) => (
+          <View key={index} style={{ flexDirection: "row" }}>
+            <Text style={{ marginHorizontal: 50, color: "#fff", fontSize: 20 }}>
+              {item}
+            </Text>
+            <Text style={{ color: "#fff", fontSize: 20 }}>
+              {timestore[index]}
+            </Text>
+          </View>
+        ))}
+      </ImageBackground>
 
-<TouchableOpacity  onPress={back} style={styles.touchable}> <Text style={styles.back}>back</Text></TouchableOpacity>
+      <TouchableOpacity onPress={back} style={styles.touchable}>
+        {" "}
+        <Text style={styles.back}>back</Text>
+      </TouchableOpacity>
     </View>
-    
-    )}
-const styles =StyleSheet .create( 
-{
- continer:{
-    padding:10,
-    flex:1,
-   marginBottom:10,
-    alignItems:'center',
-   justifyContent:'center',
-    backgroundColor:'#080f61',
- },
- touchable:{
-  width:70,
-  height:40,
-  borderRadius:20,
-  borderColor:'#ffff',
-  backgroundColor:'#fff',
-  marginTop:90,
-  
- },
-text:{
-fontSize:26,
-color:'#fff',
-marginBottom:30,
-width:'90%',
- flexDirection:'row',
-
-},
-back:{
-    color:'#201919',
-    fontWeight:'bold',
-    padding:6,
-    marginBottom:10,
-    textAlign:'center',
-    
-},
-title:{
-    fontSize:20,
-    fontWeight:'bold',
-    color:'#fff',
-marginBottom:30,
-textAlign:'top',
-justifyContent:'flex-start',
-marginHorizontal:40,
- 
-} ,
-titlel:{
-    fontSize:20,
-    fontWeight:'bold',
-    color:'#fff',
-marginBottom:10,
-textAlign:'top',
- 
-},
-image: {
+  );
+}
+const styles = StyleSheet.create({
+  continer: {
+    padding: 10,
+    flex: 1,
+    marginBottom: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#080f61",
+  },
+  touchable: {
+    width: 70,
+    height: 40,
+    borderRadius: 20,
+    borderColor: "#ffff",
+    backgroundColor: "#fff",
+    marginTop: 90,
+  },
+  text: {
+    fontSize: 26,
+    color: "#fff",
+    marginBottom: 30,
+    width: "90%",
+    flexDirection: "row",
+  },
+  back: {
+    color: "#201919",
+    fontWeight: "bold",
+    padding: 6,
+    marginBottom: 10,
+    textAlign: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#fff",
+    marginBottom: 30,
+    textAlign: "top",
+    justifyContent: "flex-start",
+    marginHorizontal: 40,
+  },
+  titlel: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#fff",
+    marginBottom: 10,
+    textAlign: "top",
+  },
+  image: {
     width: 200,
     height: 150,
     marginBottom: 20,
-  }
-
-})
+  },
+});
---
+++ src/app/(tabs)/_layout.js
@@ -0,0 +1,38 @@
+import { Tabs } from "expo-router";
+import { FontAwesome5 } from "@expo/vector-icons";
+import { TaskProveder } from "../../context/taskcontext";
+export default function Layout() {
+  return (
+    <TaskProveder>
+      <Tabs
+        screenOptions={{
+          tabBarStyle: {
+            setOffset: 0,
+            borderTopWidth: 0,
+          },
+        }}
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: "Home",
+            headerShown: false,
+            tabBarIcon: ({ size, color }) => (
+              <FontAwesome5 name="home" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="recored"
+          options={{
+            title: "Recored",
+            headerShown: false,
+            tabBarIcon: ({ size, color }) => (
+              <FontAwesome5 name="record-vinyl" size={size} color={color} />
+            ),
+          }}
+        />
+      </Tabs>
+    </TaskProveder>
+  );
+}
